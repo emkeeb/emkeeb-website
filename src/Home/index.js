16b551927b504389a7e067a8c0f4217d29a61f02
Emkeeb <emkeeb.business@gmail.com>
2022-06-07 21:21:45 -0500

    embedded youtube video and more responsiveness

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,8 +2,10 @@ import { useNavigate } from "react-router-dom";
 import {
   StyledPageLandingBlock,
   StyledLandingPageHeader,
-  LandingPageContentWrapper,
   StyledSpanBorder,
+  StyledInfoContainer,
+  StyledYoutubeInfo,
+  StyledYoutubeIframe,
 } from "./styles";
 import { ButtonPrimary, ButtonSecondary } from "../GlobalStyles";
 import LandingPageCard from "../Components/LandingPageCard";
@@ -17,48 +19,64 @@ const Home = () => {
 
   return (
     <div>
-      <StyledPageLandingBlock>
-        <LandingPageContentWrapper>
-          <LandingPageCard />
-          <StyledLandingPageHeader>
-            <div className="font-black font-size-humongus margin-bottom-xl">
-              Hello
-            </div>
-            <div className="font-size-sub-header font-regular-italic margin-bottom-xl margin-top-xl">
-              <span className="margin-right-lg">keyboard builder</span>
-              <StyledSpanBorder className="padding-left-lg">
-                software engineer
-              </StyledSpanBorder>
-            </div>
-            <div className="margin-top-xl margin-bottom-xl">
-              <ButtonPrimary
-                onClick={() => navigateTo("build-service")}
-                className="margin-right-lg margin-bottom-sm font-size-body"
-              >
-                build service
-              </ButtonPrimary>
-              <ButtonSecondary
-                onClick={() => navigateTo("")}
-                className="font-size-body"
-              >
-                contact me
-              </ButtonSecondary>
-            </div>
-            <div className="font-light font-line-height-x2 padding-top-xs">
-              <p className="font-size-body">
-                I have a passion for tech and keyboards, and love creating
-                content for both!
-              </p>
-              <p className="font-size-body">
-                I build & review keyboards, I take commissions for keyboard
-                builds for <b>FREE</b>. If you're interested in owning an
-                awesome keyboard with a personal build experience click the
-                build service button above!
-              </p>
-            </div>
-          </StyledLandingPageHeader>
-        </LandingPageContentWrapper>
-      </StyledPageLandingBlock>
+      <StyledPageLandingBlock />
+      <StyledInfoContainer $isFirstContainer={true}>
+        <LandingPageCard />
+        <StyledLandingPageHeader>
+          <div className="font-black font-size-humongus margin-bottom-xl">
+            Hello
+          </div>
+          <div className="font-size-sub-header font-regular-italic margin-bottom-xl margin-top-xl">
+            <span className="margin-right-lg">keyboard builder</span>
+            <StyledSpanBorder className="padding-left-lg">
+              software engineer
+            </StyledSpanBorder>
+          </div>
+          <div className="margin-top-xl margin-bottom-xl">
+            <ButtonPrimary
+              onClick={() => navigateTo("build-service")}
+              className="margin-right-lg margin-bottom-sm font-size-body"
+            >
+              build service
+            </ButtonPrimary>
+            <ButtonSecondary
+              onClick={() => navigateTo("")}
+              className="font-size-body"
+            >
+              contact me
+            </ButtonSecondary>
+          </div>
+          <div className="font-light font-line-height-x2 padding-top-xs">
+            <p className="font-size-body">
+              I have a passion for tech and keyboards, and love creating content
+              for both!
+            </p>
+            <p className="font-size-body">
+              I build & review keyboards, I take commissions for keyboard builds
+              for <b>FREE</b>. If you're interested in owning an awesome
+              keyboard with a personal build experience click the build service
+              button above!
+            </p>
+          </div>
+        </StyledLandingPageHeader>
+      </StyledInfoContainer>
+      <StyledInfoContainer>
+        <StyledYoutubeIframe>
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/txdmSHIstes"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </StyledYoutubeIframe>
+        <StyledYoutubeInfo className="font-size-header font-black">
+          {" "}
+          Watch my latest video here!
+        </StyledYoutubeInfo>
+      </StyledInfoContainer>
     </div>
   );
 };
